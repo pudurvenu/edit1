c9b3fc8c098a8579af1f870b89ba7b8cd93fdc0b
pudurvenu <pudurvenugopal97@gmail.com>
2021-12-04 13:21:43 +0530

    fffffffffffff

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<?php
+
+$color = $_POST["colortext"];
+$color = $_POST["textsize"];
+$text = $_POST["para"];
+
+echo $color;
+echo $text;
+
+ ?>
+
+
+
+  </body>
+</html>
